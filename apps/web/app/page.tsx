08c4ef0f879c60a549f51a54fbce52d8f7a88b01
sabdawork <sabdadeveloper@gmail.com>
2025-05-27 16:28:19 +0800

    prettier config

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,10 @@
-import { ListButton } from "@repo/ui/list-button";
-import React from "react";
+import { ListButton } from '@repo/ui/list-button';
+import React from 'react';
 
 export default function Page() {
   return (
     <div>
-      <h1 className="text-3xl underline font-bold">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <ListButton />
     </div>
   );
