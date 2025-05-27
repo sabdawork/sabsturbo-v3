81be2f36478ade4f63b3880f0998122b220c10a2
sabdawork <sabdadeveloper@gmail.com>
2025-05-27 16:11:20 +0800

    initial commit

---
+++ apps/docs/app/page.tsx
@@ -0,0 +1,11 @@
+import { ListButton } from "@repo/ui/list-button";
+import React from "react";
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-3xl underline font-bold">Hello world!</h1>
+      <ListButton />
+    </div>
+  );
+}
---
+++ packages/ui/src/list-button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export function ListButton() {
+  return (
+    <div>
+      <button className="btn btn-soft">Default</button>
+      <button className="btn btn-soft btn-primary">Primary</button>
+      <button className="btn btn-soft btn-secondary">Secondary</button>
+      <button className="btn btn-soft btn-accent">Accent</button>
+      <button className="btn btn-soft btn-info">Info</button>
+      <button className="btn btn-soft btn-success">Success</button>
+      <button className="btn btn-soft btn-warning">Warning</button>
+      <button className="btn btn-soft btn-error">Error</button>
+    </div>
+  );
+}
